aae32b50e73d5dfc8757fc321238f7787f85dc27
Juani <juaniforclaz55@gmail.com>
2023-04-12 00:13:38 -0300

    Splitter Terminado

--- main.js
+++ main.js
@@ -1,18 +1,50 @@
-let total = document.getElementById("total");
-let newTotal = document.getElementById("newTotal");
-let resultadoTotal = document.getElementsByClassName("resultado");
-let personas = document.getElementById("personas");
-let newPersonas = document.getElementById("newPersonas");
+const totalInput = document.getElementById("total");
+const propinaInput = document.getElementById("propina");
+const personasInput = document.getElementById("personas");
+const newTotalOutput = document.getElementById("newTotal");
+const newPersonasOutput = document.getElementById("newPersonas");
+const compartirButton = document.getElementById("compartir");
+const resultadoTexto = document.getElementById("resultadoTexto");
 
-total.addEventListener("input", showTotal);
-personas.addEventListener("input", showPersonas);
+function calcularPropina() {
+  const total = parseFloat(totalInput.value);
+  const propina = parseFloat(propinaInput.value);
+  const personas = parseFloat(personasInput.value);
 
-function showTotal(e) {
-  cantTotal = newTotal.textContent = e.target.value;
+  // Validar que los valores sean números
+  if (isNaN(total) || isNaN(propina) || isNaN(personas)) {
+    newTotalOutput.textContent = "---";
+    newPersonasOutput.textContent = "---";
+    resultadoTexto.value = "";
+    return;
+  }
+
+  const propinaCalculada = total * (propina / 100);
+  const totalConPropina = total + propinaCalculada;
+  const totalPorPersona = totalConPropina / personas;
+
+  newTotalOutput.textContent = totalConPropina.toFixed(2);
+  newPersonasOutput.textContent = totalPorPersona.toFixed(2);
+
+  // Generar texto a copiar para compartir
+  const texto = `El valor total es: $${total.toFixed(
+    2
+  )}\nEl porcentaje de propina es: ${propina}%\nEl valor total con propina es: $${totalConPropina.toFixed(
+    2
+  )}\nEl valor total por persona es: $${totalPorPersona.toFixed(2)}`;
+  resultadoTexto.value = texto;
 }
 
-function showPersonas(e) {
-  cantPersonas = newPersonas.textContent = e.target.value;
-  totalPorPersona = newTotal.textContent / newPersonas.textContent;
-  newPersonas.innerHTML = totalPorPersona;
+totalInput.addEventListener("input", calcularPropina);
+propinaInput.addEventListener("input", calcularPropina);
+personasInput.addEventListener("input", calcularPropina);
+
+// Función para copiar el texto generado al portapapeles
+function copiarTexto() {
+  resultadoTexto.select();
+  resultadoTexto.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("El texto ha sido copiado correctamente!");
 }
+
+compartirButton.addEventListener("click", copiarTexto);
